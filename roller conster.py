021e71c1021c6429eaa66735f306932c963b169e
Shahzaibsalman <163665494+Shahzaibsalman@users.noreply.github.com>
2025-12-28 13:33:06 +0500

    Add files via upload

---
+++ roller conster.py
@@ -0,0 +1,10 @@
+from selectors import SelectSelector
+
+print("             wall come to roller coster         ")
+print("enter a heght")
+heght = int(input("enter a heght: "))
+if heght > 100:
+    print("wall come to roller conter")
+print("enter a age")
+age = int(input("enter a age"))
+ f age >12 and age < 15:
